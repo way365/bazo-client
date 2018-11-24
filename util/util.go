7b31085f3505218481e3274a8e639ce386065e85
febe19 <fabio.maddaloni@uzh.ch>
2018-11-24 16:10:54 +0100

    performance Logger file for client

--- util/util.go
+++ util/util.go
@@ -1,10 +1,19 @@
 package util
 
 import (
+	"io"
 	"log"
 	"os"
 )
 
 func InitLogger() *log.Logger {
-	return log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	performanceLogFile, err := os.OpenFile("PerformanceLoggerClient.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+
+	wrt := io.MultiWriter(os.Stdout, performanceLogFile)
+	log.SetOutput(wrt)
+	return log.New(wrt, "INFO: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
 }
